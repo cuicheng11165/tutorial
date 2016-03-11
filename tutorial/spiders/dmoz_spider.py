bacd91ba5789d1df989979ce71971ac7b4a7bbce
Cheng Cui <cuicheng11165@gmail.com>
2016-03-11 10:12:24 +0800

    Add remove pyc

--- tutorial/spiders/dmoz_spider.py
+++ tutorial/spiders/dmoz_spider.py
@@ -5,26 +5,26 @@ from scrapy.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
 from tutorial.items import Website
 
-
-
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["www.cnblogs.com"]
-    start_urls = ["http://www.cnblogs.com/artech/"]
+    start_urls = ["http://www.cnblogs.com/TomXu/"]
 
-    output_location = 'D:\\Crawl\\cnblog\\Artech\\'
+    output_location = 'D:\\Crawl\\cnblog\\TomXu\\'
 
     def parse(self, response):
 
         s = response.url.strip('/')
-        pos = s.rfind("/")
-        filename = self.output_location + s[pos + 1:len(s) - 1]
-  
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        if s.find('?') < 0:
+            pos = s.rfind("/")
+            filename = self.output_location + s[pos + 1:len(s) - 1]  
+            with open(filename, 'wb') as f:
+                f.write(response.body)
+        
+ 
 
         sel = scrapy.Selector(response)
         for url in response.xpath('//a/@href').extract():
-            if(url.find('artech') > 0):
+            if(url.find('TomXu') > 0):
                 yield scrapy.Request(url, callback=self.parse)
          
